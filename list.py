31ae02c7cba16f97ebc3fd43578565dfd025885b
minthukhant78 <minthukhant2001@outlook.com>
2023-06-24 22:58:22 +0630

    list

--- list.py
+++ list.py
@@ -1,4 +1,12 @@
 countries = [ 'UK', 'US', 'My', 'JV']
-# print(countries[0:3])
+print(countries[0:3])
+# out ['UK', 'US', 'My']
+countries[0]='UAS'
+print (countries)
+# out ['UAS', 'US', 'My', 'JV']
+# နေရာလဲ
+print (len(countries))
+# out 4
+# အရေအတွက်ကို ပြောတာပါ
 print (type(countries))
 # out (class list )
