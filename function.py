eb60254f0e5c834f5f63df8b491aaad53a7a5fda
minthukhant78 <minthukhant2001@outlook.com>
2023-06-24 23:56:04 +0630

    function

---
+++ function.py
@@ -0,0 +1,9 @@
+def start_function(name,age,Location):
+    print('ကျွန်တော်အမည် က '+ name , '.','I am ',str(age), 'old years.' ,'My Location is ',Location)
+    # ပြန်မခါ်ပေးရင်ဘာမမှာမတွက်လာဘူး
+    # function ပြန်ခေါါ်ပေးရတယ်
+name = input ('Enther Your Name : ')
+age = input ('Enter Your Age : ')
+Location = input ('Enter Your Location :')
+start_function(name,age,Location)
+# out <Welcome User>
