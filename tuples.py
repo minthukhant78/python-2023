e4caaca15bb669e9059cb0b446e046fdcb14cac4
minthukhant78 <minthukhant2001@outlook.com>
2023-06-24 23:24:03 +0630

    tuples

---
+++ tuples.py
@@ -0,0 +1,10 @@
+tree_number = (1,2,3)
+# tree_number[1] = 23
+print(tree_number)
+# out (1, 2, 3)
+print(tree_number[1])
+# out (2)
+print (type(tree_number))
+# <class 'tuple'>
+print(tree_number)
+# TypeError: 'tuple' object does not support item assignment
