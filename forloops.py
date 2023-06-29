24b35b0931d0e5437b458d7c695c962d59c07e45
minthukhant78 <minthukhant2001@outlook.com>
2023-06-30 06:00:49 +0630

    forloops

---
+++ forloops.py
@@ -0,0 +1,12 @@
+mylist = ['Min','Thu','Khant']
+
+for values in mylist :
+    print(values)
+    # out အားလုံးပြန်ထုက်ပေး။
+    if values == 'Thu':
+        break
+    # out ရပ်ပေး။
+for x in range(4):
+    print(x)
+else :
+    print('Finished Looping!!')
