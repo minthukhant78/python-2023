1c580311f5e6bce5509273e9f0f1061660c07aa6
minthukhant78 <minthukhant2001@outlook.com>
2023-06-30 06:36:49 +0630

    try

---
+++ tryexcept.py
@@ -0,0 +1,9 @@
+try:
+    x = int (input ('Input an Integer : ' ))
+    print(x)
+except:
+    print('Somthing went wrong .. Please try Again')
+else :
+    print("nothing went wrong")
+
+# x ရဲ့တန်းဖိုးက int တာ ဘဲ ဖြစ်ရမယ်။
