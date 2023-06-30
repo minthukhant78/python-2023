82f4fd983645dbb7d5e2af1e4889c21fb1b363d3
minthukhant78 <minthukhant2001@outlook.com>
2023-06-30 14:01:35 +0630

    Inheritance in Python

---
+++ Inheritance in Python/app.py
@@ -0,0 +1,7 @@
+from new import my 
+class Person (my):
+    pass
+p1 = my()
+print("My Name is ",p1.name)
+print("I am ",p1.age,'is old')
+print('I Work is',p1.work)
---
+++ Inheritance in Python/new.py
@@ -0,0 +1,4 @@
+class my () :
+    name = str(input('Enter Your Name : '))
+    age = int(input('Enter Your Age : '))
+    work = input('Enter Your Work : ')
