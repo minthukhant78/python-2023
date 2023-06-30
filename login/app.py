126b8560d7f126ffa8572cf841c7b533cbf3512b
minthukhant78 <minthukhant2001@outlook.com>
2023-06-30 14:20:00 +0630

    login

---
+++ login/app.py
@@ -0,0 +1,15 @@
+print('Login Account Now')
+
+usernmae = input ('Enter usernmae : ')
+password = input ('Enter Password : ')
+
+print ('Your Account has been create successfully ')
+print ('Long Now ')
+
+usernmae2 = input ('Enter usernmae : ')
+password2= input ('Enter Password : ')
+
+if usernmae == usernmae2  and password == password2:
+    print ('Logged in successfully ')
+else :
+    print ('မှားနေပါတယ်')
